c62be973b1e2b5988bac7788b9f4cec94346b2b3
hongbing <hongbinghp@163.com>
2019-09-06 11:02:08 +0800

    hot fix一阶段调试成功

--- 32_hotfix/src/main/java/com/hencoder/hotfix/MainActivity.java
+++ 32_hotfix/src/main/java/com/hencoder/hotfix/MainActivity.java
@@ -9,10 +9,7 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Field;
 
-import dalvik.system.BaseDexClassLoader;
-import dalvik.system.PathClassLoader;
 import okio.BufferedSink;
 import okio.Okio;
 import okio.Source;
@@ -35,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         hotfixBt = findViewById(R.id.hotfixBt);
 
         // /data/user/0/com.hencoder.hotfix/cache/32_hotfix-debug.apk
-//        apk = new File(getCacheDir() + "/32_hotfix-debug.apk"); 由于getCacheDir()拿到的文件目录是上面那个，但实际文件目录是当前行这个，所以此处把文件目录写死，确保能正确读取文件
-        apk = new File("/data/data/com.hencoder.hotfix/cache" + "/32_hotfix-debug.apk");
+        apk = new File(getCacheDir() + "/32_hotfix-debug.apk");  // 由于getCacheDir()拿到的文件目录是上面那个，但实际文件目录是当前行这个，所以此处把文件目录写死，确保能正确读取文件
+//        apk = new File("/data/data/com.hencoder.hotfix/cache" + "/32_hotfix-debug.apk");
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -95,44 +92,44 @@ public class MainActivity extends AppCompatActivity {
 //                            e.printStackTrace();
 //                        }
 
-                        try {
-                            ClassLoader classLoader = getClassLoader();
-                            Class loaderClass = BaseDexClassLoader.class;
-                            Field pathListField = loaderClass.getDeclaredField("pathList");
-                            pathListField.setAccessible(true);
-                            Object pathListObject = pathListField.get(classLoader); // getClassLoader().pathList
-                            System.out.println("pathListObject：" + pathListObject);
-                            Class pathListClass = pathListObject.getClass();
-                            Field dexElementsField = pathListClass.getDeclaredField("dexElements");
-                            dexElementsField.setAccessible(true);
-                            Object dexElementsObject = dexElementsField.get(pathListObject); // getClassLoader().pathList.dexElements
-
-                            // classLoader.pathList.dexElements = ???;
-//                            System.out.println("apk path：" + apk.getPath());
-                            PathClassLoader newClassLoader = new PathClassLoader(apk.getPath(), null);
-                            Object newPathListObject = pathListField.get(newClassLoader); // newClassLoader.pathList
-                            System.out.println("newPathListObject：" + newPathListObject);
-                            Object newDexElementsObject = dexElementsField.get(newPathListObject); // newClassLoader.pathList.dexElements
-                            System.out.println("newDexElementsObject：" + newDexElementsObject);
-
-                            dexElementsField.set(pathListObject, newDexElementsObject);
-
-//                            int oldLength = Array.getLength(dexElementsObject);
-//                            int newLength = Array.getLength(newDexElementsObject);
-//                            Object concatDexElementsObject = Array.newInstance(dexElementsObject.getClass().getComponentType(), oldLength + newLength);
-//                            for (int i = 0; i < newLength; i++) {
-//                                Array.set(concatDexElementsObject, i, Array.get(newDexElementsObject, i));
-//                            }
-//                            for (int i = 0; i < oldLength; i++) {
-//                                Array.set(concatDexElementsObject, newLength + i, Array.get(dexElementsObject, i));
-//                            }
+//                        try {
+//                            ClassLoader classLoader = getClassLoader();
+//                            Class loaderClass = BaseDexClassLoader.class;
+//                            Field pathListField = loaderClass.getDeclaredField("pathList");
+//                            pathListField.setAccessible(true);
+//                            Object pathListObject = pathListField.get(classLoader); // getClassLoader().pathList
+//                            System.out.println("pathListObject：" + pathListObject);
+//                            Class pathListClass = pathListObject.getClass();
+//                            Field dexElementsField = pathListClass.getDeclaredField("dexElements");
+//                            dexElementsField.setAccessible(true);
+//                            Object dexElementsObject = dexElementsField.get(pathListObject); // getClassLoader().pathList.dexElements
 //
-//                            dexElementsField.set(pathListObject, concatDexElementsObject);
-                        } catch (NoSuchFieldException e) {
-                            e.printStackTrace();
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
+//                            // classLoader.pathList.dexElements = ???;
+////                            System.out.println("apk path：" + apk.getPath());
+//                            PathClassLoader newClassLoader = new PathClassLoader(apk.getPath(), null);
+//                            Object newPathListObject = pathListField.get(newClassLoader); // newClassLoader.pathList
+//                            System.out.println("newPathListObject：" + newPathListObject);
+//                            Object newDexElementsObject = dexElementsField.get(newPathListObject); // newClassLoader.pathList.dexElements
+//                            System.out.println("newDexElementsObject：" + newDexElementsObject);
+//
+//                            dexElementsField.set(pathListObject, newDexElementsObject);
+//
+////                            int oldLength = Array.getLength(dexElementsObject);
+////                            int newLength = Array.getLength(newDexElementsObject);
+////                            Object concatDexElementsObject = Array.newInstance(dexElementsObject.getClass().getComponentType(), oldLength + newLength);
+////                            for (int i = 0; i < newLength; i++) {
+////                                Array.set(concatDexElementsObject, i, Array.get(newDexElementsObject, i));
+////                            }
+////                            for (int i = 0; i < oldLength; i++) {
+////                                Array.set(concatDexElementsObject, newLength + i, Array.get(dexElementsObject, i));
+////                            }
+////
+////                            dexElementsField.set(pathListObject, concatDexElementsObject);
+//                        } catch (NoSuchFieldException e) {
+//                            e.printStackTrace();
+//                        } catch (IllegalAccessException e) {
+//                            e.printStackTrace();
+//                        }
 
                         break;
                     default:
